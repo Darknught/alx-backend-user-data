18eb8574f626fc08f2e370c8a7003ae24918bd30
Darknught <hassino66@yahoo.com>
2024-07-04 10:40:37 +0000

    code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,3 +26,36 @@ def filter_datum(
             lambda match: f"{match.group().split('=')[0]}={redaction}",
             message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class to filter values in log records
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with fields to be obfuscated.
+
+        Args:
+            fields (List[str]): List of field names to be obfuscated.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record, obfuscating specified fields.
+
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted log record with specified fields obfuscated.
+        """
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+                self.fields, self.REDACTION, original_message, self.SEPARATOR)
