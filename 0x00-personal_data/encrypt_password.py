e30af6790c1fc257ee7d38800685199d04de863c
Darknught <hassino66@yahoo.com>
2024-07-04 13:50:45 +0000

    checking validity of hased password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> bytes:
     """ Method to hash the password."""
     hashed = bcrypt.hashpw(password.encode('utf-8'),  bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Method that checks if hashed password is valid."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
