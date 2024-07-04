e2b63c5fbfcffeaf9aee45da4102b971646f71da
Darknught <hassino66@yahoo.com>
2024-07-04 19:45:28 +0000

    code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -89,9 +89,9 @@ def get_db() -> MySQLConnection:
     Returns:
         MySQLConnection: A MySQL connection object.
     """
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     database = os.getenv('PERSONAL_DATA_DB_NAME')
 
     return mysql.connector.connect(
