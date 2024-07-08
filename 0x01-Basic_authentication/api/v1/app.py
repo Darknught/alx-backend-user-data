746b69951e06435c3a7d6b62a0a2551470141645
Darknught <hassino66@yahoo.com>
2024-07-08 17:14:30 +0000

    code

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+import json
+from flask import Response
 
 
 app = Flask(__name__)
@@ -19,6 +21,27 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+def pretty_jsonify(data):
+    """ Helper function to return pretty-printed JSON response """
+    return Response(json.dumps(data, indent=2), mimetype='application/json')
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+    Error: Unauthorized handler
+    """
+    return pretty_jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    Error: forbidden handler
+    """
+    return pretty_jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
