8edcc4dc63abb163c4fa796c83bab595172d2122
Darknught <hassino66@yahoo.com>
2024-07-09 09:55:48 +0000

    code

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -40,7 +40,11 @@ class Auth:
             request - None
         Return: string
         """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ A method that determines the current user
