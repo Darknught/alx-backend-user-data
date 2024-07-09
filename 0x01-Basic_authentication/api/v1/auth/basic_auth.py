6bc667110bc79ef1a2b926c34850737a0d81be16
Darknught <hassino66@yahoo.com>
2024-07-09 11:40:43 +0000

    code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,3 +36,19 @@ class BasicAuth(Auth):
             return dcoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Method that returns the user email and password
+        from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        usr_email, usr_password = decoded_base64_authorization_header.split(
+                ':', 1)
+        return usr_email, usr_password
