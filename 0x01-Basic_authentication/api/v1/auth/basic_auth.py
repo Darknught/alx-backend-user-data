6c6deaedba9848d821cdc0a5f7ff23d88b93ee41
Darknught <hassino66@yahoo.com>
2024-07-09 11:05:23 +0000

    code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ class that inherits from Auth
     """
-    
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """ A method that returns Base64 part of the
