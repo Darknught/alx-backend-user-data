fce4845e09dc666449721e87b0be13dd60ad6ba5
Darknught <hassino66@yahoo.com>
2024-07-09 11:30:55 +0000

    code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ A new class that inherits
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ method that returns decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            dcoded_bytes = base64.b64decode(base64_authorization_header)
+            return dcoded_bytes.decode('utf-8')
+        except Exception:
+            return None
