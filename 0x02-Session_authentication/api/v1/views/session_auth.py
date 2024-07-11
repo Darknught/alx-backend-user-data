4615b924f4a17f0b8ab7e9a1161faba189d04d4b
Darknught <hassino66@yahoo.com>
2024-07-11 18:19:36 +0000

    code

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,12 @@ def login():
     response = jsonify(user.to_json())
     response.set_cookie(auth.SESSION_NAME, session_id)
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Handles the logout process for session authentication
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
