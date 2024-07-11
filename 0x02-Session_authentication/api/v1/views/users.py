06f56ab96a1dcc15015cab23a2e6a9ab1ff88ba8
Darknught <hassino66@yahoo.com>
2024-07-11 11:34:22 +0000

    code

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -37,6 +37,7 @@ def view_one_user(user_id: str = None) -> str:
             abort(404)
         return jsonify(user.to_json())
 
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
@@ -124,6 +125,7 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
 
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def get_current_user() -> str:
