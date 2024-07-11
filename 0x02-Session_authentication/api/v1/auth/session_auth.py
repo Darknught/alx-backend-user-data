26a44d6651d0443dc5e6f13fec02020b6fe24a6f
Darknught <hassino66@yahoo.com>
2024-07-11 12:04:19 +0000

    code

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" class that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth that inherits from Auth
+    """
+    pass
