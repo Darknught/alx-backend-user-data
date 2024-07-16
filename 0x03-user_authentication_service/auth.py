d8b8833376403dd0f10d15da705763cdcc283ce4
Darknught <hassino66@yahoo.com>
2024-07-16 13:00:28 +0000

    Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,19 +7,20 @@ from db import DB, User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """ Method to return the hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """ Method to return the hashed password
-        """
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """ Method that takes in str args and return User object
