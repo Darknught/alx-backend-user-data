3a4084c3928086ab98eb3960a4bd3a3d7d49514b
Darknught <hassino66@yahoo.com>
2024-07-16 20:47:25 +0000

    Login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,6 +2,7 @@
 """ Module that defines a flask app
 """
 from flask import Flask, jsonify, request
+from flask import abort, make_response
 from auth import Auth
 
 
@@ -37,6 +38,31 @@ def users() -> str:
 
     except Exception:
         return ({"message": "email already registered"}), 400
+
+
+@app.route('/sessions/', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions
+    Return:
+        Json Payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = make_response(jsonify(
+        {"email": "<user email>", "message": "logged in"}))
+
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
