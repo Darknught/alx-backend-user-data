22fe73f1cd129f3307310627ec120850e56195c7
Darknught <hassino66@yahoo.com>
2024-07-16 14:06:44 +0000

    updating flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ Module that defines a flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -14,6 +17,26 @@ def home() -> str:
         Json payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users/', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users/
+    Return:
+        Json Payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "email and password required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except Exception:
+        return ({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
