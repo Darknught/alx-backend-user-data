14b5e6a8a42c72e437e136722b483082732458dc
Darknught <hassino66@yahoo.com>
2024-07-16 16:24:24 +0000

    code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """ Method that returns a string representation of new UUID
     """
     return str(uuid.uuid4())
