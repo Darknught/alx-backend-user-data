88897647e8d37c04cd5f79cb697d1bf21e1cc3e1
Darknught <hassino66@yahoo.com>
2024-07-17 08:21:06 +0000

    code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,11 @@ class Auth:
             return new_user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Method that destroys a session
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
