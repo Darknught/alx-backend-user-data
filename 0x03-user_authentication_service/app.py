362a3a62c443938ab7d1c36cc662cd10af9875cd
Darknught <hassino66@yahoo.com>
2024-07-17 12:42:46 +0000

    code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -120,8 +120,10 @@ def get_reset_password_token():
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """ PUT /reset_password
+    Return:
+        Json Payload indicating password updated
     """
-    email = request.form.get("emaiil")
+    email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
 
